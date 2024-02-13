a39938432ea5f0fbb6f06f949996bcdedef8a62b
leydi montero <leidy_940@hotmail.es>
2024-02-12 20:16:48 -0500

    base del pryecto e instalación bootstrap

---
+++ proyecto-integrador-CTD/src/App.jsx
@@ -0,0 +1,24 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'animate.css/animate.min.css';
+import './App.css'
+
+function App() {
+  return (
+    <div className="container mt-4"> 
+      <h1>Hola!!</h1>
+      <p>inicio del proyecto integrador.</p>
+      <div className="mb-2"> 
+        <div className="mr-2"> 
+          
+          <button type="button" className="btn btn-success">Botón de éxito</button>
+        </div>
+        <div> 
+          
+          <button type="button" className="btn btn-primary btn-sm animate__animated animate__bounce">Animación</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App
