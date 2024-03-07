0b90fd21126a3b707c91e9debd26c83fc6b3702c
wendyrodriguez15 <yulianagarciawr@gmail.com>
2024-03-07 15:21:07 -0500

    avatar

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,22 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import Avatar from "./Avatar";
 
 const Header = () => {
+
+
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    // Aquí tomamos el token que está almacenado en localStorage
+    const storedToken = localStorage.getItem('token');
+
+    if (storedToken) {
+      setToken(storedToken);
+    }
+  }, []);
+
+
   return (
     <header className="fixed left-0 right-0 top-0 z-40 py-4 px-5 md:px-8 bg-colorClaro flex items-center justify-between">
       <div className="flex items-center">
@@ -14,19 +30,30 @@ const Header = () => {
       </div>
 
       <div className="flex gap-2 md:gap-4">
-        <Link
-          to="/registro"
-          className="px-2 py-1 md:px-4 text-base rounded-full bg-colorPrimario text-white"
-        >
-          Crear cuenta
-        </Link>
-        <Link
-          to="/login"
-          className="px-2 py-1 md:px-4 text-base rounded-full bg-colorPrimario text-white"
-        >
-          Iniciar Sesion
-        </Link>
+
+
+        {token ? (
+          <Avatar />
+        ) : (
+          <>
+            <Link
+              to="/registro"
+              className="px-2 py-1 md:px-4 text-base rounded-full bg-colorPrimario text-white"
+            >
+              Crear cuenta
+            </Link>
+            <Link
+              to="/login"
+              className="px-2 py-1 md:px-4 text-base rounded-full bg-colorPrimario text-white"
+            >
+              Iniciar Sesion
+            </Link>
+          </>
+        )}
+
+
       </div>
+
     </header>
   );
 };
